909082bdcbb421eccbb6e5fd4e8b38f6b3697ee0
Kenaii11 <78361355+Kenaii11@users.noreply.github.com>
2022-04-24 20:48:00 +0200

    Update Hub.lua

--- Hub.lua
+++ Hub.lua
@@ -5,7 +5,7 @@ loadstring(game:HttpGet(''))() -- Ninja Legends 2
 elseif game.PlaceId == 000000000 then
 loadstring(game:HttpGet(''))() -- Prison life 
 elseif game.PlaceId == 000000000 then
-loadstring(game:HttpGet(''))() -- Speed Legends
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Kenaii11/projects/main/speedlegends'))() -- Speed Legends
 elseif game.PlaceId == 000000000 then 
 loadstring(game:HttpGet(''))() -- Da Hood
 elseif game.PlaceId == 000000000 then
